ab3b6766aaa165b83f04f1b8d9efc94b12ef24a8
TheBreadEnergy <50107547+TheBreadEnergy@users.noreply.github.com>
2019-07-18 01:21:55 +0300

    так красивше

--- gonfalonier/Panel.gd
+++ gonfalonier/Panel.gd
@@ -3,7 +3,11 @@ extends Panel
 func _ready():
 	pass
 
-func _on_Button_pressed():
+func _on_Step_pressed():
 	var num = randi()%6+1
-	get_node("Label").set_text(str(num))
+	get_node("шаг").set_text(str(num))
 	pass 
+
+
+func _on_End_pressed():
+	pass # Replace with function body.
